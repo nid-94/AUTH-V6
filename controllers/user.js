9cc45692c51061c02b8e458956cca24e9351ae49
nid-94 <86736538+nid-94@users.noreply.github.com>
2022-05-14 12:25:44 +0100

    uplooad

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
-const SECRETKEY = "123";
+
 
 exports.signup = async (req, res) => {
     try {
